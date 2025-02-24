500a199bb7f665420639af6798bd857af44a5b39
Juan Merino <juan.merino@jemersoft.com>
2025-02-24 11:27:08 -0300

    ref: remove swiper

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -3,12 +3,12 @@
     v-if="tags?.length"
     class="mt-4"
   >
-    <div class="swiper tags__swiper">
-      <div class="swiper-wrapper">
+    <div>
+      <div>
         <div
           v-for="(tag, index) in tags"
           :key="index"
-          class="swiper-slide tags__swiper-slide mx-1"
+          class="inline-block m-1"
         >
           <a
             class="btn btn-sm btn-soft"
@@ -21,31 +21,12 @@
   </section>
 </template>
 
-<style>
-  .tags__swiper-slide {
-    width: auto !important;
-  }
-</style>
-
 <script setup>
   import { selectedTags } from '@src/store';
   import { useStore } from '@nanostores/vue';
-  import { watchEffect } from 'vue';
-  import Swiper from 'swiper';
 
   const $selectedTags = useStore(selectedTags);
   const { tags } = defineProps(['tags']);
-
-  watchEffect(() => {
-    if (tags?.length) {
-      setTimeout(() => {
-        new Swiper('.tags__swiper', {
-          slidesPerView: 'auto',
-          spaceBetween: 0,
-        });
-      }, 0);
-    }
-  });
 
   function toggleTag(tag) {
     if ($selectedTags.value.includes(tag)) {
