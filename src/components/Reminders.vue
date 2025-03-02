961b4248b8abc80afbb88f31215589b50303036d
Juan Merino <juan.merino@jemersoft.com>
2025-03-02 18:21:18 -0300

    ref: add reminders update

--- src/components/Reminders.vue
+++ src/components/Reminders.vue
@@ -11,84 +11,109 @@
 	<article v-else>
 		<Tags :tags="reminderUser?.tags" />
 		<section class="mt-4">
-			<div v-for="reminder in filteredReminders" :key="reminder.id"
-				class="card rounded-sm bg-base-200 shadow-sm p-4 mb-4">
-				<div class="text-end pb-1">
-					<div v-if="reminder.created" class="badge badge-primary">
-						{{ getStringDate(reminder.created) }}
-					</div>
-					<div class="dropdown dropdown-end ms-3">
-						<div ref="dropdown-button" tabindex="0" role="button" class="btn btn-circle btn-soft btn-xs rounded-btn">
-							<svg class="w-6 h-6 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-								<path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M6 12h.01m6 0h.01m5.99 0h.01"/>
-							</svg>
+			<div v-if="!filteredReminders?.length" role="alert" class="alert">
+				<svg class="shrink-0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+					<path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+					<path d="M12 9h.01" />
+					<path d="M11 12h1v4h1" />
+				</svg>
+				<span>No se encontraron Reminders</span>
+			</div>
+			<div v-else>
+				<div
+					v-for="reminder in filteredReminders" :key="reminder.id"
+					class="card rounded-sm bg-base-200 shadow-sm p-4 mb-4"
+				>
+					<div class="text-end pb-1">
+						<!-- <div v-if="reminder.created" class="badge badge-primary">
+							{{ getStringDate(reminder.created) }}
+						</div> -->
+						<div class="dropdown dropdown-end ms-3">
+							<div ref="dropdown-button" class="btn btn-circle btn-soft btn-xs rounded-btn" tabindex="0" role="button" aria-label="Abrir opciones del recordatorio">
+								<svg class="w-6 h-6 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+									<path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M6 12h.01m6 0h.01m5.99 0h.01"/>
+								</svg>
+							</div>
+							<ul
+								tabindex="0"
+								class="menu dropdown-content bg-base-100 rounded-box w-32 mt-2 p-2 shadow"
+							>
+								<li @click="openAddOrUpdateReminderModal(reminder)">
+									<button class="btn btn-soft btn-sm" aria-label="Editar recordatorio">
+										<svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+											<path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+											<path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+											<path d="M16 5l3 3" />
+										</svg>
+										<span>Editar</span>
+									</button>
+								</li>
+								<li @click="deleteReminder(reminder.id)">
+									<button class="btn btn-error btn-soft btn-sm mt-1" aria-label="Eliminar recordatorio">
+										<svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+											<path d="M4 7l16 0" />
+											<path d="M10 11l0 6" />
+											<path d="M14 11l0 6" />
+											<path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+											<path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+										</svg>
+										<span>Eliminar</span>
+									</button>
+								</li>
+							</ul>
 						</div>
-						<ul
-							tabindex="0"
-							class="menu dropdown-content bg-base-100 rounded-box w-32 mt-2 p-2 shadow"
-						>
-							<li @click="deleteReminder(reminder.id)">
-								<a class="btn btn-error btn-soft btn-sm">
-									<svg class="w-4 h-4 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
-									</svg>
-									<span>Eliminar</span>
-								</a>
-							</li>
-						</ul>
 					</div>
-				</div>
-				<div>
-					<a v-if="reminder.link" :href="reminder.link" target="blank" class="btn btn-link p-0">{{ reminder.name }}</a>
-					<span v-else>{{ reminder.name }}</span>
-				</div>
-				<div v-if="reminder.tags?.length" class="mt-4">
-					<div v-for="(tag, index) in reminder.tags" :key="index" class="badge badge-soft me-2">
-						{{ tag }}
+					<div>
+						<a v-if="reminder.link" :href="reminder.link" target="blank" class="btn btn-link p-0">{{ reminder.name }}</a>
+						<span v-else>{{ reminder.name }}</span>
+					</div>
+					<div v-if="reminder.tags?.length" class="mt-4">
+						<div v-for="(tag, index) in reminder.tags" :key="index" class="badge badge-soft me-2">
+							{{ tag }}
+						</div>
 					</div>
 				</div>
 			</div>
 			<div class="fixed bottom-0 left-0 bg-transparent w-full z-[1000]">
 				<div class="max-w-5xl mx-auto text-end p-4 pe-8">
-					<button class="btn btn-soft btn-circle btn-lg me-3" @click="goTop">
-						<svg class="w-6 h-6 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-							height="24" fill="none" viewBox="0 0 24 24">
-							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-								d="M12 6v13m0-13 4 4m-4-4-4 4" />
+					<button class="btn btn-soft btn-circle btn-lg me-3" aria-label="Volver al principio" @click="goTop">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+							<path d="M6 15l6 -6l6 6" />
 						</svg>
 					</button>
-					<button class="btn btn-primary btn-circle btn-lg" @click="openAddReminderModal">
-						<svg class="w-6 h-6 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-							height="24" fill="none" viewBox="0 0 24 24">
-							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-								d="M5 12h14m-7 7V5" />
+					<button class="btn btn-primary btn-circle btn-lg" aria-label="Agregar recordatorio" @click="openAddOrUpdateReminderModal(null)">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+							<path d="M12 5l0 14" />
+							<path d="M5 12l14 0" />
 						</svg>
 					</button>
 				</div>
 			</div>
-			<dialog ref="add-reminder-modal" class="modal modal-bottom sm:modal-middle">
+			<dialog ref="add-or-update-reminder-modal" class="modal modal-bottom sm:modal-middle">
 				<div class="modal-box">
 					<form method="dialog">
-						<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+						<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" aria-label="Cerrar modal">✕</button>
 					</form>
-					<h3 class="text-lg font-bold">Agregar nuevo Reminder</h3>
-					<div class="mt-4">
-						<input v-model.trim="name" type="text" placeholder="Nombre*" class="input input-bordered w-full mt-2" />
-						<input v-model.trim="link" type="text" placeholder="Links" class="input input-bordered w-full mt-2" />
-						<input v-model.trim="tags" type="text" placeholder="Etiquetas" class="input input-bordered w-full mt-2" />
-						<div class="mt-3">
-							<small class="text-gray-400">(*) Obligatorios</small><br>
-							<small class="text-gray-400">Etiquetas: texto separado por comas</small>
+					<form method="dialog" @submit="addOrUpdateReminder">
+						<h3 class="text-lg font-bold">{{editedReminder ? 'Editar Reminder' : 'Agregar nuevo Reminder'}}</h3>
+						<div class="mt-4">
+							<input v-model.trim="name" type="text" placeholder="Nombre*" class="input input-bordered w-full mt-2" />
+							<input v-model.trim="link" type="text" placeholder="Links" class="input input-bordered w-full mt-2" />
+							<input v-model.trim="tags" type="text" placeholder="Etiquetas" class="input input-bordered w-full mt-2" />
+							<div class="mt-3">
+								<small class="text-gray-400">(*) Obligatorios</small><br>
+								<small class="text-gray-400">Etiquetas: texto separado por comas</small>
+							</div>
 						</div>
-					</div>
-					<div class="modal-action">
-						<form method="dialog">
-							<button class="btn btn-primary" :disabled="!name" @click="addReminder">Agregar</button>
-						</form>
-					</div>
+						<div class="modal-action">
+							<button type="submit" class="btn btn-primary" aria-label="Guardar recordatorio" :disabled="!name">
+								{{editedReminder ? 'Editar' : 'Agregar'}}
+							</button>
+						</div>
+					</form>
 				</div>
 				<form method="dialog" class="modal-backdrop">
-					<button>close</button>
+					<button aria-label="Cerrar modal">close</button>
 				</form>
 			</dialog>
 		</section>
@@ -106,11 +131,12 @@ import { db } from '@fb/client';
 
 const $user = useStore(user);
 const $selectedTags = useStore(selectedTags);
-const addReminderModal = useTemplateRef('add-reminder-modal');
+const addOrUpdateReminderModal = useTemplateRef('add-or-update-reminder-modal');
 
 const name = ref('');
 const link = ref('');
 const tags = ref('');
+const editedReminder = ref(null);
 
 let reminderUser = ref({});
 let reminderUserPending = ref(true);
@@ -136,22 +162,26 @@ let filteredReminders = computed(() => {
 	return reminders.value.filter(r => r.tags?.length && $selectedTags.value.every(rt => r.tags.includes(rt)));
 });
 
-function getStringDate(created) {
-	const date = new Date(created);
-	let minutes = date.getMinutes();
-	minutes = minutes < 10 ? `0${minutes}` : minutes;
-	return `${date.toLocaleDateString()} ${date.getHours()}:${minutes}`;
-}
+// function getStringDate(created) {
+// 	const date = new Date(created);
+// 	let minutes = date.getMinutes();
+// 	minutes = minutes < 10 ? `0${minutes}` : minutes;
+// 	return `${date.toLocaleDateString()} ${date.getHours()}:${minutes}`;
+// }
 
 function goTop() {
 	window && window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-function openAddReminderModal() {
-	addReminderModal.value.showModal();
+function openAddOrUpdateReminderModal(reminder) {
+	editedReminder.value = reminder;
+	name.value = reminder?.name || '';
+	link.value = reminder?.link || '';
+	tags.value = reminder?.tags?.join(',') || '';
+	addOrUpdateReminderModal.value.showModal();
 }
 
-async function addReminder() {
+async function addOrUpdateReminder() {
 	if (!name.value || !$user.value) return;
 
 	const tagsList = tags.value ? [...new Set(tags.value.split(',').map(t => t ? t.trim().toLowerCase() : null).filter(t => !!t))] : null;
@@ -170,18 +200,28 @@ async function addReminder() {
 		}
 	}
 
-	batch.set(doc(collection(db, 'reminder', $user.value.uid, 'reminders')), {
-		created: new Date().getTime(),
-		name: name.value,
-		link: link.value || null,
-		tags: tagsList?.length ? tagsList : null,
-	});
+	if (editedReminder.value) {
+		batch.update(doc(db, 'reminder', $user.value.uid, 'reminders', editedReminder.value.id), {
+			updated: new Date().getTime(),
+			name: name.value,
+			link: link.value || null,
+			tags: tagsList?.length ? tagsList : null,
+		});
+	} else {
+		batch.set(doc(collection(db, 'reminder', $user.value.uid, 'reminders')), {
+			created: new Date().getTime(),
+			name: name.value,
+			link: link.value || null,
+			tags: tagsList?.length ? tagsList : null,
+		});
+	}
 
 	await batch.commit();
 
 	name.value = '';
 	link.value = '';
 	tags.value = '';
+	editedReminder.value = null;
 }
 
 async function deleteReminder(id) {
